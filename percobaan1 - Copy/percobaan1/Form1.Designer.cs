19211c0b48cfa69047a899eb9ce2f6044b18b160
Enrico Ian Hartojo <74400644+enricoian@users.noreply.github.com>
2020-11-13 15:04:27 +0700

    Add files via upload

---
+++ percobaan1 - Copy/percobaan1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace percobaan1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.namaFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sheetList = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kategoriList = new System.Windows.Forms.ComboBox();
+            this.recapText = new System.Windows.Forms.TextBox();
+            this.anggotaList = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(759, 348);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(696, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // namaFile
+            // 
+            this.namaFile.Location = new System.Drawing.Point(72, 363);
+            this.namaFile.Name = "namaFile";
+            this.namaFile.Size = new System.Drawing.Size(618, 20);
+            this.namaFile.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nama File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sheet";
+            // 
+            // sheetList
+            // 
+            this.sheetList.FormattingEnabled = true;
+            this.sheetList.Location = new System.Drawing.Point(72, 389);
+            this.sheetList.Name = "sheetList";
+            this.sheetList.Size = new System.Drawing.Size(121, 21);
+            this.sheetList.TabIndex = 5;
+            this.sheetList.SelectedIndexChanged += new System.EventHandler(this.sheetList_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 419);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Recap";
+            // 
+            // kategoriList
+            // 
+            this.kategoriList.FormattingEnabled = true;
+            this.kategoriList.Location = new System.Drawing.Point(72, 416);
+            this.kategoriList.Name = "kategoriList";
+            this.kategoriList.Size = new System.Drawing.Size(121, 21);
+            this.kategoriList.TabIndex = 7;
+            this.kategoriList.SelectedIndexChanged += new System.EventHandler(this.kategoriList_SelectedIndexChanged);
+            // 
+            // recapText
+            // 
+            this.recapText.Location = new System.Drawing.Point(72, 449);
+            this.recapText.Name = "recapText";
+            this.recapText.Size = new System.Drawing.Size(121, 20);
+            this.recapText.TabIndex = 8;
+            // 
+            // anggotaList
+            // 
+            this.anggotaList.Location = new System.Drawing.Point(199, 416);
+            this.anggotaList.Name = "anggotaList";
+            this.anggotaList.Size = new System.Drawing.Size(121, 21);
+            this.anggotaList.TabIndex = 0;
+            this.anggotaList.SelectedIndexChanged += new System.EventHandler(this.anggotaList_SelectedIndexChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 452);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Jumlah";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::percobaan1.Properties.Resources.unnamed;
+            this.pictureBox3.Location = new System.Drawing.Point(516, 392);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(121, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::percobaan1.Properties.Resources.Universitas_Airlangga_Surabaya;
+            this.pictureBox2.Location = new System.Drawing.Point(379, 389);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 57);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(643, 392);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(138, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(376, 459);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(255, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Create by Enrico and Asro from S1-Mathematic Unair";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 481);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.anggotaList);
+            this.Controls.Add(this.recapText);
+            this.Controls.Add(this.kategoriList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sheetList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namaFile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Program REKAP";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox namaFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox sheetList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox kategoriList;
+        private System.Windows.Forms.TextBox recapText;
+        private System.Windows.Forms.ComboBox anggotaList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ percobaan1 - Copy/percobaan1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+namespace percobaan1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTableCollection tabelCollection;
+        DataColumnCollection columnCollection;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Excel Worksheet|* .xlsx|Excel 97-2003 Workbook|* .xls" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    namaFile.Text = openFileDialog.FileName;
+                    using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                            });
+                            tabelCollection = result.Tables;
+
+                            sheetList.Text = "";
+                            sheetList.Items.Clear();
+
+                            kategoriList.Text = "";
+                            kategoriList.Items.Clear();
+
+                            anggotaList.Text = "";
+                            anggotaList.Items.Clear();
+
+                            recapText.Text = "";
+
+                            //tampil isi sheet
+                            foreach (DataTable table in tabelCollection)
+                                sheetList.Items.Add(table.TableName);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void sheetList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = tabelCollection[sheetList.SelectedItem.ToString()];
+            dataGridView1.DataSource = dt;
+
+            //tampil isi kolom
+            kategoriList.Items.Clear();
+            foreach (DataColumn column in dt.Columns)
+                kategoriList.Items.Add(column.ColumnName);
+
+            kategoriList.Text = "";
+
+            anggotaList.Text = "";
+            anggotaList.Items.Clear();
+
+            recapText.Text = "";
+        }
+
+        private void kategoriList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            DataTable dt = tabelCollection[sheetList.SelectedItem.ToString()];
+            dataGridView1.DataSource = dt;
+
+            string dc = kategoriList.SelectedItem.ToString();
+            string[] A = new string[dt.Rows.Count];
+            int c = 0;
+
+            anggotaList.Items.Clear();
+
+            //cek kembar
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                A[i] = dt.Rows[i][dc].ToString();
+            }
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    if (A[i] == A[j] && i != j)
+                    {
+                        A[i] = "";
+                    }
+                }
+
+                if (A[i] != "")
+                {
+                    c += 1;
+                }
+            }
+
+
+            string[] B = new string[c];
+
+            for (int i = 0; i < c; i++)
+            {
+                B[i] = "";
+            }
+
+            //tampil isi baris
+            int n = 0;
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < dt.Rows.Count; j++)
+                {
+                    if (B[i] == "")
+                    {
+                        B[i] = A[n];
+                        n += 1;
+                    }
+                }
+                anggotaList.Items.Add(B[i]);
+            }
+
+            anggotaList.Text = "";
+
+            recapText.Text = "";
+        }
+  
+        private void anggotaList_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            DataTable dt = tabelCollection[sheetList.SelectedItem.ToString()];
+            string dc = kategoriList.SelectedItem.ToString();
+            string dd = anggotaList.SelectedItem.ToString();
+            int d = 0;
+
+            DataView dv = new DataView(dt);
+            dv.Sort = dc + " desc";
+             dv.RowFilter ="[" + dc + "]" + "=" + "'" + dd + "'";
+
+            dataGridView1.DataSource = dv;
+
+            //tampil hasil jml rekap
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string a = dt.Rows[i][dc].ToString();
+                if (dd == a)
+                {
+                    d += 1;
+                } 
+            }
+
+            recapText.Text = d.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+ 
